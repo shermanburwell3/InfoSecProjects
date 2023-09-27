2bcbf6985f3687de3283d047006a7ac503cab5e5
Sherman Burwell <74177092+shermanburwell3@users.noreply.github.com>
2023-09-27 00:49:59 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -8,44 +8,6 @@ const helmet = require('helmet');
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
