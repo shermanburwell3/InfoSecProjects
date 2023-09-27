5cc0863f3d75ba56ff9ce41babc4d1bd931077ea
Sherman Burwell <74177092+shermanburwell3@users.noreply.github.com>
2023-09-27 00:22:40 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
+app.use(helmet));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log('server started on PORT ${PORT}');
+});
 
 
 
