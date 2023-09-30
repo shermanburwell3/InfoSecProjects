9abf38e7f592368f442f5baa7bad578a0dc75530
Sherman Burwell <74177092+shermanburwell3@users.noreply.github.com>
2023-09-29 23:54:49 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,22 +1,41 @@
 const express = require('express');
 const app = express();
 const helmet = require('helmet');
+/*
 app.use(helmet.hidePoweredBy());                   //removes "X-Powered-By" header
+
 app.use(helmet.frameguard({action: 'deny'}));      //prevents <frame> and <iframe> clickjacking
+
 app.use(helmet.xssFilter());                       //mitigates risk of cross site scripting (XSS) attacks
+
 app.use(helmet.noSniff());                         //avoids inferring the Response MIME Type   
+
 app.use(helmet.ieNoOpen());                        //prevents IE from opening untrusted HTML    
 
 //converting 90 days into seconds.
 let ninetyDaysInSeconds = 90 *24 * 60 * 60;
 let timeInSeconds = ninetyDaysInSeconds;
 app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));        //forces browsers to access site with HTTPS only
-app.use(helmet.dnsPrefetchControl());              //prevents browsers from storing DNS records for added security in exchange for performance
-app.use(helmet.noCache());                         //prevents caching, ensuring clients always use the newest version of the site, in exchange for performance
+
+app.use(helmet.dnsPrefetchControl());                              //prevents browsers from storing DNS records for added security in exchange for performance
+
+app.use(helmet.noCache());                                         //prevents caching, ensuring clients always use the newest version of the site, in exchange for performance
+
 app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] }} ));  //defines trusted content sources
+*/
 
-
-
+app.use(helmet ({
+  frameguard: {              //configure
+    action: 'deny'
+  },
+  contentSecurityPolicy: {    //enable and configure
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ['style.com'],
+    }
+  },
+  dnsPrefetchControl: false    //disable
+}));
 
 
 module.exports = app;
